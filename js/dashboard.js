f451f09c3f7922b20441cff57e3ea869c3884353
neverforgetter <wegosci2@gmail.com>
2017-09-24 08:22:43 -0400

    Updated styles

--- js/dashboard.js
+++ js/dashboard.js
@@ -113,7 +113,7 @@ function gotData(data) {
         var title = projects[k].title;
         var desc = projects[k].desc;
         var date = projects[k].date;
-        projectList.innerHTML += "<a href='#' id='"+keys[i]+"' onclick='viewProject(this)' class='list-group-item'>"+title.slice(0,30)+" - "+date+"<span class='float-right text-right text-secondary'>"+desc.slice(0,45)+"</span></a>";        
+        projectList.innerHTML += "<a href='#' id='"+keys[i]+"' onclick='viewProject(this)' class='list-group-item'><strong>"+title.slice(0,30)+"</strong>  Due: <strong>"+date+"</strong><span class='float-right text-right text-secondary'>"+desc.slice(0,100)+"</span></a>";        
       }
     } else {
       projectList.innerHTML = "";
